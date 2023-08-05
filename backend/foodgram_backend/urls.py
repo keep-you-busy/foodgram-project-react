5da2673f47c0c26bf86087488987e915aba70e76
keep-you-busy <119418278+keep-you-busy@users.noreply.github.com>
2023-08-05 20:06:33 +0300

    Foodgram backend stuff

---
+++ backend/foodgram_backend/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import include, path
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
+    path('api', include('djoser.urls')),
+    path('api', include('djoser.urls.auth'))
+]
