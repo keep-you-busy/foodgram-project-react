17845e9ab89c8a177a94cdbce943184ca54b1b95
keep-you-busy <119418278+keep-you-busy@users.noreply.github.com>
2023-08-09 15:56:53 +0300

    Displays reciepes, ingredients and tags in admin

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from recipes.models import Ingredient, Recipe, Tag
 
-# Register your models here.
+admin.site.register(Ingredient)
+admin.site.register(Tag)
+
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = (
+        'author',
+        'name',
+        'image',
+        'text',
+        'get_tags',
+        'get_ingretients',
+        'cooking_time'
+    )
+    empty_value_display = '-пусто-'
+
+    def get_tags(self, recipe):
+        return ', '.join(
+            tag.name for tag in recipe.tags.all()
+        )
+    
+    def get_ingretients(self, recipe):
+        return ', '.join(
+            ingredient.name for ingredient in recipe.ingredients.all()
+        )
+
+    get_tags.short_description = 'Теги'
+    get_ingretients.short_description = 'Ингредиенты'
