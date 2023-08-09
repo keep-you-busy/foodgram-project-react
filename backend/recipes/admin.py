434ea645d0b2324f6a884aa31f9799187dac73fc
keep-you-busy <119418278+keep-you-busy@users.noreply.github.com>
2023-08-09 17:19:38 +0300

    Interface for M2M tables into admin panel

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -1,9 +1,17 @@
 from django.contrib import admin
-from recipes.models import Ingredient, Recipe, Tag
+from recipes.models import Ingredient, IngredientRecipe, Recipe, Tag, TagRecipe
 
 admin.site.register(Ingredient)
 admin.site.register(Tag)
 
+
+class IngredientRecipeInline(admin.TabularInline):
+    model = IngredientRecipe
+    extra = 1
+
+class TagRecipeInline(admin.TabularInline):
+    model = TagRecipe
+    extra = 1
 
 @admin.register(Recipe)
 class RecipeAdmin(admin.ModelAdmin):
@@ -13,20 +21,21 @@ class RecipeAdmin(admin.ModelAdmin):
         'image',
         'text',
         'get_tags',
-        'get_ingretients',
+        'get_ingredients',
         'cooking_time'
     )
     empty_value_display = '-пусто-'
+    inlines = [IngredientRecipeInline, TagRecipeInline]
 
     def get_tags(self, recipe):
         return ', '.join(
             tag.name for tag in recipe.tags.all()
         )
-    
-    def get_ingretients(self, recipe):
+
+    def get_ingredients(self, recipe):
         return ', '.join(
             ingredient.name for ingredient in recipe.ingredients.all()
         )
 
     get_tags.short_description = 'Теги'
-    get_ingretients.short_description = 'Ингредиенты'
+    get_ingredients.short_description = 'Ингредиенты'
