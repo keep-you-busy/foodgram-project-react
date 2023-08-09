13e4867b3167183b1acb16cd770f6d776f1e05c7
keep-you-busy <119418278+keep-you-busy@users.noreply.github.com>
2023-08-09 16:00:02 +0300

    Associative tables for tags and ingredients between recipes

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -2,18 +2,69 @@ from django.db import models
 from users.models import User
 
 
+class Tag(models.Model):
+    """Модель тега."""
+
+    name = models.CharField(
+        verbose_name='Наименование тега',
+        unique=True,
+        max_length=250,
+        null=False,
+        blank=False,
+        default='Инициализирующий тег'
+    )
+    color = models.CharField(
+        verbose_name='Цвет тега',
+        unique=True,
+        max_length=7,
+        default=None
+    )
+    slug = models.SlugField(
+        'slug',
+        max_length=50,
+        unique=True,
+        blank=False,
+        db_index=True
+    )
+
+    def __str__(self) -> str:
+        return f'{self.name}'
+
+class Ingredient(models.Model):
+    """Модель ингредиента."""
+
+    name = models.CharField(
+        verbose_name='Наименование ингредиента',
+        unique=True,
+        max_length=250,
+        null=False,
+        blank=False,
+        default='Инициализирующий ингредиент'
+    )
+    measures = models.CharField(
+        verbose_name='Единица измерения ингредиента',
+        max_length=50,
+        null=False,
+        blank=False
+    )
+
+    def __str__(self) -> str:
+        return f'{self.name}'
+
 class Recipe(models.Model):
-    "Модель рецепта."
+    """Модель рецепта."""
 
     author = models.ForeignKey(
         User, related_name='recipe',
         verbose_name='Автор рецепта',
         on_delete=models.CASCADE,
         null=False,
-        blank=False
-    ),
+        blank=False,
+        default=0
+    )
     name = models.CharField(
         verbose_name='Наименование рецепта',
+        max_length=256,
         null=False,
         blank=False
     )
@@ -23,69 +74,49 @@ class Recipe(models.Model):
         null=False,
         blank=False,
         default=None
-    ),
+    )
     text = models.TextField(
         verbose_name='Описание рецепта',
         null=False,
-        blank=False
-    ),
+        blank=False,
+        default='Пустой текст'
+    )
     ingredients = models.ManyToManyField(
-        # Ingredient, - end the class for ingredient
+        Ingredient,
+        through='IngredientRecipe',
         verbose_name='Ингредиенты рецепта',
-        # choices = 
-        null=False,
-        blank=False
-    ),
+    )
     tags = models.ManyToManyField(
-        # Tags, - end the class fo tags
+        Tag,
         verbose_name='Теги рецепта',
-        # choices = 
-        null=False,
-        blank=False
-    ),
+    )
     cooking_time = models.DurationField(
         verbose_name='Время готовки рецепта',
         null=False,
-        blank=False
-    )
-
-class Tag(models.Model):
-    "Модель тега."
-
-    name = models.CharField(
-        verbose_name='Наименование тега',
-        unique=True,
-        null=False,
-        blank=False
-    ),
-    color = models.CharField(
-        verbose_name='Цвет тега',
-        max_length=16
-    ),
-    slug = models.SlugField(
-        'slug',
-        max_length=50,
-        unique=True,
         blank=False,
-        db_index=True
+        default=0
     )
 
-class Ingredient(models.Model):
-    "Модель ингредиента."
+    def __str__(self) -> str:
+        return f'{self.name}'
 
-    name = models.CharField(
-        verbose_name='Наименование ингредиента',
-        unique=True,
-        null=False,
-        blank=False
-    ),
-    count = models.IntegerField(
-        verbose_name='Количество ингредиента',
-        null=False,
-        blank=False
+class IngredientRecipe(models.Model):
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.PROTECT)
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    quantity = models.DecimalField(
+        verbose_name='Количество',
+        max_digits=10,
+        decimal_places=2,
+        default=0
+
     )
-    measures = models.CharField(
-        verbose_name='Единица измерения ингредиента',
-        null=False,
-        blank=False
-    )
+
+    def __str__(self) -> str:
+        return f'{self.ingredient} {self.recipe}'
+
+class TagRecipe(models.Model):
+    tag = models.ForeignKey(Tag, on_delete=models.PROTECT)
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return f'{self.tag} {self.recipe}'
