101c5e778662b6bc32092f1386611743a9e92f40
keep-you-busy <119418278+keep-you-busy@users.noreply.github.com>
2023-08-13 19:58:16 +0300

    Action for me root for the current user

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,13 +1,38 @@
 from api.serializers import (IngredientSerializer, RecipeSerializer,
                              TagSerializer, UserSerializer)
 from recipes.models import Ingredient, Recipe, Tag
-from rest_framework import viewsets
+from rest_framework import permissions, status, viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from users.models import User
 
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly, )
+
+    @action(
+        methods=['GET', 'PATCH'],
+        detail=False,
+        permission_classes=(permissions.IsAuthenticated,),
+        url_path='me')
+    def get_current_user_info(self, request):
+        serializer = UserSerializer(request.user, context={'request': request})
+        if request.method == 'PATCH':
+            serializer = UserSerializer(
+                request.user,
+                data=request.data,
+                partial=True,
+                context={'request': request}
+                )
+
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+        return Response(serializer.data)
+
 
 class TagViewSet(viewsets.ModelViewSet):
     queryset = Tag.objects.all()
