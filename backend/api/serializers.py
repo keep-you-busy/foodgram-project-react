5c208d39b37dfc743261db0f72b5ac51e5604996
keep-you-busy <119418278+keep-you-busy@users.noreply.github.com>
2023-08-13 19:57:48 +0300

    Subscribe field in api, write only permission for password

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,19 +1,33 @@
+from django.contrib.auth.hashers import make_password
 from recipes.models import Ingredient, Recipe, Tag
 from rest_framework import serializers
-from users.models import User
+from users.models import Follow, User
 
 
 class UserSerializer(serializers.ModelSerializer):
+    is_subscribed = serializers.SerializerMethodField()
+    password = serializers.CharField(
+        write_only=True, style={'input_type': 'password'}
+    )
 
     class Meta:
         model = User
         fields = (
             'email',
+            'id',
             'username',
             'first_name',
             'last_name',
+            'is_subscribed',
             'password'
         )
+    read_only_fields = ("is_subscribed",)
+
+    def get_is_subscribed(self, obj):
+        user = self.context.get('request').user
+        if user.is_anonymous:
+            return False
+        return Follow.objects.filter(user=user, author=obj).exists()
 
 class TagSerializer(serializers.ModelSerializer):
 
