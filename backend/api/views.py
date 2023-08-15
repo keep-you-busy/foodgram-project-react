08abe156fe76cf59cbe2823131cc407ad0101b90
keep-you-busy <119418278+keep-you-busy@users.noreply.github.com>
2023-08-15 18:19:22 +0300

    New child View Set from djoser package

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,5 +1,6 @@
-from api.serializers import (IngredientSerializer, RecipeSerializer,
-                             TagSerializer, UserSerializer)
+from api.serializers import (CustomUserSerializer, IngredientSerializer,
+                             RecipeSerializer, TagSerializer)
+from djoser.views import UserViewSet
 from recipes.models import Ingredient, Recipe, Tag
 from rest_framework import permissions, status, viewsets
 from rest_framework.decorators import action
@@ -7,40 +8,20 @@ from rest_framework.response import Response
 from users.models import User
 
 
-class UserViewSet(viewsets.ModelViewSet):
+class CustomUserViewSet(UserViewSet):
     queryset = User.objects.all()
-    serializer_class = UserSerializer
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly, )
-
-    @action(
-        methods=['GET', 'PATCH'],
-        detail=False,
-        permission_classes=(permissions.IsAuthenticated,),
-        url_path='me')
-    def get_current_user_info(self, request):
-        serializer = UserSerializer(request.user, context={'request': request})
-        if request.method == 'PATCH':
-            serializer = UserSerializer(
-                request.user,
-                data=request.data,
-                partial=True,
-                context={'request': request}
-                )
-
-            serializer.is_valid(raise_exception=True)
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)
-
-        return Response(serializer.data)
+    serializer_class = CustomUserSerializer
 
 
 class TagViewSet(viewsets.ModelViewSet):
     queryset = Tag.objects.all()
     serializer_class = TagSerializer
+    pagination_class = None
 
 class IngredientViewSet(viewsets.ModelViewSet):
     queryset = Ingredient.objects.all()
     serializer_class = IngredientSerializer
+    pagination_class = None
 
 class RecipeViewSet(viewsets.ModelViewSet):
     queryset = Recipe.objects.all()
