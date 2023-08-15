fd14121e0fb94a8539e9b15eb7cf6335ef4fbb82
keep-you-busy <119418278+keep-you-busy@users.noreply.github.com>
2023-08-15 18:16:58 +0300

    Custom fields for recipe serializer, read only check for ingredients and tags

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -29,27 +29,47 @@ class CustomUserSerializer(UserSerializer):
             return False
         return Follow.objects.filter(user=user, author=obj).exists()
 
+
 class TagSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Tag
         fields = ('__all__')
+        read_only_fields = ("__all__",)
 
 class IngredientSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Ingredient
         fields = ('__all__')
+        read_only_fields = ("__all__",)
 
 class RecipeSerializer(serializers.ModelSerializer):
+    is_favorited = serializers.SerializerMethodField()
+    tags = TagSerializer(many=True)
+    author = CustomUserSerializer()
+    ingredients = IngredientSerializer(many=True)
+
 
     class Meta:
         model = Recipe
         fields = (
-            'ingredients',
+            'id',
             'tags',
-            'image',
+            'author',
+            'ingredients',
+            'is_favorited',
             'name',
+            'image',
             'text',
             'cooking_time',
         )
+
+    def get_is_favorited(self, obj):
+        user = self.context.get('request').user
+        if user.is_anonymous:
+            return False
+        return Favorites.objects.filter(user=user, author=obj).exists()
+
+    #def get_ingredients(self, obj):
+    # take care of getting list of the ingredients
