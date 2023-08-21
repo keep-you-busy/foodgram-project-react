4541a633fd9bfa57f1b9dfda8fbe777133c398ba
keep-you-busy <119418278+keep-you-busy@users.noreply.github.com>
2023-08-21 17:30:47 +0300

    Custom exceptions for post delete actions in subscribe and favorite method

---
+++ backend/core/exceptions.py
@@ -0,0 +1,7 @@
+class ObjectExistsError(Exception):
+    """Исключение из-за наличия объектов при методе DELETE."""
+    pass
+
+class ObjectNotFoundError(Exception):
+    """Исключение из-за отсутствия объектов при методе POST."""
+    pass
