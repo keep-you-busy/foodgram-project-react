3b23d7ef42ca13b69377bc55a1b70e9b70e689c1
keep-you-busy <119418278+keep-you-busy@users.noreply.github.com>
2023-08-24 19:42:52 +0300

    Custom pagination

---
+++ backend/api/pagination.py
@@ -0,0 +1,10 @@
+from rest_framework.pagination import PageNumberPagination
+
+
+class CustomPagination(PageNumberPagination):
+    page_size_query_param = 'limit'
+    recipes_limit_query_param = 'recipes_limit'
+
+    def paginate_queryset(self, queryset, request, view=None):
+        self.recipes_limit = request.query_params.get(self.recipes_limit_query_param)
+        return super().paginate_queryset(queryset, request, view)
