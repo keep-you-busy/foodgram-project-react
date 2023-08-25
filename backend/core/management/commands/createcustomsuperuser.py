bf88bf2ea06a4281820d7d5a9cd5b020010c6061
keep-you-busy <119418278+keep-you-busy@users.noreply.github.com>
2023-08-25 20:51:43 +0300

    More functional method for custom create superuser

--- backend/core/management/commands/createcustomsuperuser.py
+++ backend/core/management/commands/createcustomsuperuser.py
@@ -11,15 +11,22 @@ class Command(BaseCommand):
     help = 'Creates basic a superuser if not exists'
     requires_migrations_checks = True
 
-    def handle(self, *args, **options):
-        if not User.objects.filter(username='admin').exists():
-            User.objects.create_superuser(
+    @staticmethod
+    def create_custom_superuser(user_model):
+        if not user_model.objects.filter(username='admin').exists():
+            user_model.objects.create_superuser(
                 email=os.getenv('SUPERUSER_EMAIL', 'admin@admin.com'),
                 username=os.getenv('SUPERUSER_USERNAME', 'admin'),
                 first_name=os.getenv('SUPERUSER_FIRST_NAME', 'admin'),
                 last_name=os.getenv('SUPERUSER_LAST_NAME', 'admin'),
                 password=os.getenv('SUPERUSER_PASSWORD', 'admin')
             )
+            return True
+        else:
+            return False
+
+    def handle(self, *args, **options):
+        if self.create_custom_superuser(User):
             self.stdout.write(
                 self.style.SUCCESS(
                     'Superuser created successfully'
