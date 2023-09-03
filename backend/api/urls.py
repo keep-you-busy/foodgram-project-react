00319c6bdec50f0ad7927f90e67c3b163ddaf3a0
keep-you-busy <119418278+keep-you-busy@users.noreply.github.com>
2023-09-03 19:33:41 +0300

    router instace with version

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,17 +1,19 @@
-from api.views import (CustomUserViewSet, IngredientViewSet, RecipeViewSet,
-                       TagViewSet)
 from django.urls import include, path
 from rest_framework import routers
 
-router = routers.DefaultRouter()
+from api.views import (CustomUserViewSet, IngredientViewSet, RecipeViewSet,
+                       TagViewSet)
 
-router.register(r'users', CustomUserViewSet, basename='users')
-router.register(r'tags', TagViewSet, basename='tags')
-router.register(r'recipes', RecipeViewSet, basename='recipes')
-router.register(r'ingredients', IngredientViewSet, basename='ingredients')
+
+router_v1 = routers.DefaultRouter()
+
+router_v1.register(r'users', CustomUserViewSet, basename='users')
+router_v1.register(r'tags', TagViewSet, basename='tags')
+router_v1.register(r'recipes', RecipeViewSet, basename='recipes')
+router_v1.register(r'ingredients', IngredientViewSet, basename='ingredients')
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path('', include(router_v1.urls)),
     path('', include('djoser.urls')),
-    path(r'auth/', include('djoser.urls.authtoken')),
+    path('auth/', include('djoser.urls.authtoken')),
 ]
