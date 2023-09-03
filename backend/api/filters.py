6a44df6fa09e7971613c0401d1393ba353891a93
keep-you-busy <119418278+keep-you-busy@users.noreply.github.com>
2023-09-03 19:31:29 +0300

    Short way for flagged methods

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -1,4 +1,6 @@
+from django.db.models import Q
 from django_filters import rest_framework as filters
+
 from recipes.models import Ingredient, Recipe
 
 
@@ -6,10 +8,12 @@ class RecipeFilter(filters.FilterSet):
     """Фильтр для модели рецепта."""
 
     is_favorited = filters.BooleanFilter(
-        method='get_filter_is_favorited'
+        method='get_filter_by_relationship',
+        field_name='in_favorites__user'
     )
     is_in_shopping_cart = filters.BooleanFilter(
-        method='get_filter_is_in_shopping_cart'
+        method='get_filter_by_relationship',
+        field_name='in_carts__user'
     )
     author = filters.NumberFilter()
     tags = filters.CharFilter(field_name='tags__slug')
@@ -21,16 +25,13 @@ class RecipeFilter(filters.FilterSet):
             'tags',
         )
 
-    def get_filter_is_favorited(self, queryset, request, view):
+    def get_filter_by_relationship(self, queryset, name, value):
         user = self.request.user
         if user.is_authenticated:
-            return queryset.filter(in_favorites__user=self.request.user)
-        return queryset
-
-    def get_filter_is_in_shopping_cart(self, queryset, request, view):
-        user = self.request.user
-        if user.is_authenticated:
-            return queryset.filter(in_carts__user=self.request.user)
+            filter_parameters = {name: user}
+            return queryset.filter(
+                Q(**filter_parameters) if value else ~Q(**filter_parameters)
+            )
         return queryset
 
 
