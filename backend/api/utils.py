c6877eeeb6ca16543c3af1b827c86f93519c525d
keep-you-busy <119418278+keep-you-busy@users.noreply.github.com>
2023-09-08 14:30:45 +0300

    Function now is more preferable

--- backend/api/utils.py
+++ backend/api/utils.py
@@ -1,16 +1,8 @@
-from functools import wraps
-
-
-def check_is_flagged(model, relation_parameter):
-    def decorator(func):
-        @wraps(func)
-        def wrapper(self, obj):
-            user = self.context.get('request').user
-            parameters = {
-                'user': user,
-                relation_parameter: obj
-            }
-            return user.is_authenticated and model.objects.filter(
-                **parameters).exists()
-        return wrapper
-    return decorator
+def check_is_flagged(model, relation_parameter, user, obj):
+    """Метод проверяет выбранное отношение между пользователем и объектом."""
+    parameters = {
+        'user': user,
+        relation_parameter: obj
+    }
+    return user.is_authenticated and model.objects.filter(
+        **parameters).exists()
